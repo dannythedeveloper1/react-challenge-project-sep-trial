1a9232d60e3cee6b2d513ec2cb0a365cfddbcb06
dannythedeveloper1 <daniel.girma9@gmail.com>
2021-10-28 21:56:50 -0400

    feat(nav): display user email in navbar

--- application/src/components/nav/nav.js
+++ application/src/components/nav/nav.js
@@ -1,16 +1,20 @@
 import React from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import { logoutUser } from "../../redux/actions/authActions";
 import "./nav.css";
 
 const Nav = (props) => {
+    const auth = useSelector(state => state.auth)
     const dispatch = useDispatch();
     const handleLogOut = () => {
         dispatch(logoutUser());
     }
     return (
         <div className="nav-strip">
+            <div className="nav-link-style nav-user">
+                <label className="nav-label">{auth.email}</label>
+            </div>
             <Link to={"/order"} className="nav-link">
                 <div className="nav-link-style">
                     <label className="nav-label">Order Form</label>
